1b9d049138b2c54a6e5468488eee325f3b70a136
ulewu <jojo@ulewu.de>
2025-06-27 07:05:11 +0200

    assembly version

--- Mail2Ticket/Properties/AssemblyInfo.cs
+++ Mail2Ticket/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Security;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.*")]
 
